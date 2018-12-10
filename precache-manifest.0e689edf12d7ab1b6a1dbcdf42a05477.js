3f3f81f96fa4287e2b443f8cee1874d15628dbb7
Delagardi <delagardi@ymail.com>
2018-12-10 11:28:29 +0200

    fix favicon

--- precache-manifest.0e689edf12d7ab1b6a1dbcdf42a05477.js
+++ precache-manifest.0e689edf12d7ab1b6a1dbcdf42a05477.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "4362c2788be2cdbeed91",
-    "url": "/static/js/main.4362c278.chunk.js"
+    "revision": "360ba78c3b0ec452709e",
+    "url": "/static/js/main.360ba78c.chunk.js"
   },
   {
     "revision": "ac57f3af89562633be95",
     "url": "/static/js/1.ac57f3af.chunk.js"
   },
   {
-    "revision": "4362c2788be2cdbeed91",
-    "url": "/static/css/main.859423bc.chunk.css"
+    "revision": "360ba78c3b0ec452709e",
+    "url": "/static/css/main.5b13f849.chunk.css"
   },
   {
-    "revision": "ac0e68ca5eaabcffa46c48af8754fff5",
+    "revision": "5f5a900eaf463bbfa9e91c485621c5df",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.46b86af6ae6ba7fb64802ac4ffbd2701.js"
+  "/precache-manifest.0e689edf12d7ab1b6a1dbcdf42a05477.js"
 );
 
 workbox.clientsClaim();
