068396e4caf6eb0a1c69b505afccc40edec55a1a
Delagardi <delagardi@ymail.com>
2018-12-10 10:41:19 +0200

    First Build Done

---
+++ precache-manifest.9871eb2c1ec5f2d316e43ff5d7a75ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4362c2788be2cdbeed91",
+    "url": "/static/js/main.4362c278.chunk.js"
+  },
+  {
+    "revision": "ac57f3af89562633be95",
+    "url": "/static/js/1.ac57f3af.chunk.js"
+  },
+  {
+    "revision": "4362c2788be2cdbeed91",
+    "url": "/static/css/main.859423bc.chunk.css"
+  },
+  {
+    "revision": "ac0e68ca5eaabcffa46c48af8754fff5",
+    "url": "/index.html"
+  }
+];
